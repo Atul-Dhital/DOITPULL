80838d906f19dd5cf40d0016162b4cc272c2e787
Atul Dhital <dhitalatul4@gmail.com>
2022-11-23 17:14:13 +0545

    fitst

---
+++ wp-content/themes/default/front-page.php
@@ -0,0 +1,330 @@
+<?php
+get_header();
+the_post();
+?>
+<main class="home">
+    <section class="slider">
+        <div class="container">
+            <div class="slickslider">
+                <div class="slider">
+                    <div class="slider__content">
+                        <h1 class="slider__heading">Launching Mobile Application
+                        </h1>
+                        <p>Honorable prime minister, K.P Sharma Oli Launching Mobile Application Local Agencies) (Dhulikhel) prepared by Department of Information Technology on August 03, 2018</p>
+                        <div class="slider__slide">
+                            <svg class="icon icon-leftArrow">
+                                <use xlink:href="#icon-leftArrow"></use>
+                            </svg>
+                            <svg class="icon icon-Path">
+                                <use xlink:href="#icon-Path"></use>
+                            </svg>
+                        </div>
+                    </div>
+                    <figure class="slider__media">
+                        <img src="<?php echo get_template_directory_uri() ?>/uploads/kpbaa.jpg" alt="image of kp oli" />
+                    </figure>
+                </div>
+                <div class="slider">
+                    <div class="slider__content">
+                        <h1 class="slider__heading">Launching Mobile Application
+                        </h1>
+                        <p>Honorable prime minister, K.P Sharma Oli Launching Mobile Application Local Agencies) (Dhulikhel) prepared by Department of Information Technology on August 03, 2018</p>
+                        <div class="slider__slide">
+                            <svg class="icon icon-leftArrow">
+                                <use xlink:href="#icon-leftArrow"></use>
+                            </svg>
+                            <svg class="icon icon-rightArrow">
+                                <use xlink:href="#icon-rightArrow"></use>
+                            </svg>
+                        </div>
+                    </div>
+                    <figure class="slider__media">
+                        <img src="<?php echo get_template_directory_uri() ?>/uploads/kpbaa.jpg" alt="image of kp oli" />
+                    </figure>
+                </div>
+                <div class="slider">
+                    <div class="slider__content">
+                        <h1 class="slider__heading">Launching Mobile Application</h1>
+                        <p>Honorable prime minister, K.P Sharma Oli Launching Mobile Application Local Agencies) (Dhulikhel) prepared by Department of Information Technology on August 03, 2018</p>
+                        <div class="slider__slide">
+                            <svg class="icon icon-leftArrow">
+                                <use xlink:href="#icon-leftArrow"></use>
+                            </svg>
+                            <svg class="icon icon-rightArrow">
+                                <use xlink:href="#icon-rightArrow"></use>
+                            </svg>
+                        </div>
+                    </div>
+                    <figure class="slider__media">
+                        <img src="<?php echo get_template_directory_uri() ?>/uploads/kpbaa.jpg" alt="image of kp oli" />
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="post">
+        <div class="container">
+            <div class="post__left">
+                <h2 class="post__heading">Latest Notices:</h2>
+                <p>Information of Department of IT as per RTI Act Section 5(From Magh1 to End of Chaitra 2076)</p>
+            </div>
+            <div class="post__right">
+                <div class="post__profile">
+                    <figure class="post__image">
+                        <img src="<?php echo get_template_directory_uri() ?>/uploads/profile1.jpg" alt="profile of a person" />
+                    </figure>
+                    <div class="post__profile-detail">
+                        <h3 class="post__profile-title">Prem Sharan Shrestha</h3>
+                        <p>Director General</p>
+                    </div>
+                </div>
+                <div class="post__profile">
+                    <figure class="post__image">
+                        <img src="<?php echo get_template_directory_uri() ?>/uploads/profile3.jpg" alt="profile of a person" />
+                    </figure>
+                    <div class="post__profile-detail">
+                        <h3 class="post__profile-title">Tej Kumar K.C.</h3>
+                        <p>Information Officer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="article">
+        <div class="container">
+            <div class="article__item">
+                <figure class="article__media">
+                    <img src="<?php echo get_template_directory_uri() ?>/uploads/block1.jpg" alt="image of a article" />
+                </figure>
+                <div class="article__content">
+                    <h2 class="article__heading">IT Infrastructure</h2>
+                    <p>Information and Communication Technology (ICT) together with Internet is making it possible to share vast amount of knowledge and information and …</p>
+                    <div class="article__link">
+                        <a href="#">Read More</a>
+                        <svg class="icon icon-arrow">
+                            <use xlink:href="#icon-arrow"></use>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div class="article__item">
+                <figure class="article__media">
+                    <img src="<?php echo get_template_directory_uri() ?>/uploads/block2.jpg" alt="image of a article" />
+                </figure>
+                <div class="article__content">
+                    <h2 class="article__heading">Cyber Security Awareness</h2>
+                    <p>Information and Communication Technology (ICT) together with Internet is making it possible to share vast amount of knowledge and information and …</p>
+                    <div class="article__link">
+                        <a href="#">Read More</a>
+                        <svg class="icon icon-arrow">
+                            <use xlink:href="#icon-arrow"></use>
+                        </svg>
+                    </div>
+                </div>
+
+            </div>
+            <div class="article__item">
+                <figure class="article__media">
+                    <img src="<?php echo get_template_directory_uri() ?>/uploads/block3.jpg" alt="image of a article" />
+                </figure>
+                <div class="article__content">
+                    <h2 class="article__heading">E Governance</h2>
+                    <p>Information and Communication Technology (ICT) together with Internet is making it possible to share vast amount of knowledge and information and …</p>
+                    <div class="article__link">
+                        <a href="#">Read More</a>
+                        <svg class="icon icon-arrow">
+                            <use xlink:href="#icon-arrow"></use>
+                        </svg>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <section class="program">
+        <div class="container">
+            <h2 class="program__title">Our Programs</h2>
+            <div class="program__rows">
+                <div class="program__content">
+                    <svg class="icon icon-cloud">
+                        <use xlink:href="#icon-cloud"></use>
+                    </svg>
+                    <h3 class="program__subtitle">G Cloud</h3>
+                    <p>The advancement in technology has made people more inclined towards tablets…</p>
+                </div>
+                <div class="program__content">
+                    <svg class="icon icon-attendance">
+                        <use xlink:href="#icon-attendance"></use>
+                    </svg>
+                    <h3 class="program__subtitle">E Attendance</h3>
+                    <p>The advancement in technology has made people more inclined towards tablets…</p>
+                </div>
+                <div class="program__content">
+                    <svg class="icon icon-profile">
+                        <use xlink:href="#icon-profile"></use>
+                    </svg>
+                    <h3 class="program__subtitle">IT Profiling</h3>
+                    <p>In current scenario, almost all the government organization are designing and developing …</p>
+                </div>
+                <div class="program__content">
+                    <svg class="icon icon-village">
+                        <use xlink:href="#icon-village"></use>
+                    </svg>
+                    <h3 class="program__subtitle">E-Village</h3>
+                    <p>The advancement in technology has made people more inclined towards tablets…</p>
+                </div>
+                <div class="program__content">
+                    <svg class="icon icon-training">
+                        <use xlink:href="#icon-training"></use>
+                    </svg>
+                    <h3 class="program__subtitle">Training</h3>
+                    <p>The advancement in technology has made people more inclined towards tablets…</p>
+                </div>
+                <div class="program__content">
+                    <svg class="icon icon-school">
+                        <use xlink:href="#icon-school"></use>
+                    </svg>
+                    <h3 class="program__subtitle">E-School</h3>
+                    <p>The advancement in technology has made people more inclined towards tablets…</p>
+                </div>
+                <div class="program__content">
+                    <svg class="icon icon-Eattendance">
+                        <use xlink:href="#icon-Eattendance"></use>
+                    </svg>
+                    <h3 class="program__subtitle">E-Attendance</h3>
+                    <p>The advancement in technology has made people more inclined towards tablets…</p>
+                </div>
+                <div class="program__content">
+                    <svg class="icon icon-Evillage">
+                        <use xlink:href="#icon-Evillage"></use>
+                    </svg>
+                    <h3 class="program__subtitle">E-Village</h3>
+                    <p>The advancement in technology has made people more inclined towards tablets…</p>
+                </div>
+                <div class="program__content">
+                    <svg class="icon icon-digital">
+                        <use xlink:href="#icon-digital"></use>
+                    </svg>
+                    <h3 class="program__subtitle">Digital Content</h3>
+                    <p>The advancement in technology has made people more inclined towards tablets…</p>
+                </div>
+                <div class="program__content">
+                    <svg class="icon icon-service">
+                        <use xlink:href="#icon-service"></use>
+                    </svg>
+                    <h3 class="program__subtitle">Integrated IT Service Center</h3>
+                    <p>The advancement in technology has made people more inclined towards tablets…</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="news">
+        <div class="container">
+            <div class="news__left">
+                <div class="news__lists">
+                    <button class="news__btn content__btn" onclick="openCity(event, 'News')">News</button>
+                    <button class="news__btn content__btn" onclick="openCity(event, 'Press')">Press Release</button>
+                    <button class="news__btn content__btn" onclick="openCity(event, 'Events')">Upcoming Events</button>
+                    <button class="news__btn content__btn" onclick="openCity(event, 'Notices')">Notices</button>
+                </div>
+                <div id="News" class="news__tabs content__tabs">
+                    <div class="news__contents">
+                        <p>Press Release(Local Level Mobile Application)-(2075-09-23)</p>
+                        <div class="news__link">
+                            <a href="#">Read More<svg class="icon icon-arrow">
+                                    <use xlink:href="#icon-arrow"></use>
+                                </svg></a>
+                        </div>
+                    </div>
+                    <div class="news__contents">
+                        <p>Press Release(Local Level Mobile Application)-(2075-09-23)</p>
+                        <div class="news__link">
+                            <a href="#">Read More<svg class="icon icon-arrow">
+                                    <use xlink:href="#icon-arrow"></use>
+                                </svg></a>
+
+                        </div>
+                    </div>
+                    <div class="news__contents">
+                        <p>Information of Department of IT as per RTI Act Section 5(From Magh1 to End of Chaitra 2077)</p>
+                        <div class="news__link">
+                            <a href="#">Read More<svg class="icon icon-arrow">
+                                    <use xlink:href="#icon-arrow"></use>
+                                </svg></a>
+                        </div>
+                    </div>
+                    <div class="news__contents">
+                        <p>Press Release(Local Level Mobile Application)-(2075-09-23)</p>
+                        <div class="news__link">
+                            <a href="#">Read More<svg class="icon icon-arrow">
+                                    <use xlink:href="#icon-arrow"></use>
+                                </svg></a>
+                        </div>
+                    </div>
+                </div>
+                <div id="Events" class="news__tabs content__tabs">
+                    <div class="news__contents">
+                        <p>Press Release(Local Level Mobile Application)-(2075-09-23)</p>
+                        <div class="news__link">
+                            <a href="#"> Read More<svg class="icon icon-arrow">
+                                    <use xlink:href="#icon-arrow"></use>
+                                </svg></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="news__right">
+                <fieldset>
+                    <legend>Notices</legend>
+                    <div class="news__data">
+                        <h3 class="news__title">30 May 2021</h3>
+                        <p>National ICT Day- 2078</p>
+                    </div>
+                    <div class="news__data">
+                        <h3 class="news__title">30 May 2021</h3>
+                        <p>Information of Department of IT as per RTI Act Section 5(From Kartik 1 to End of Poush2077)</p>
+                    </div>
+                    <div class="news__data">
+                        <h3 class="news__title">30 May 2021</h3>
+                        <p>Information of Department of IT as per RTI Act Section 5(From Kartik 1 to End of Poush2077)</p>
+                    </div>
+                    <div class="news__data">
+                        <h3 class="news__title">30 May 2021</h3>
+                        <p>National ICT Day- 2078</p>
+                    </div>
+                </fieldset>
+            </div>
+        </div>
+    </section>
+    <section class="gallery">
+        <div class="container">
+            <div class="gallery__heading">
+                <h2 class="gallery__title">Photo Gallery</h2>
+                <a href="#">View All<svg class="icon icon-arrow">
+                        <use xlink:href="#icon-arrow"></use>
+                    </svg></a>
+            </div>
+            <div class="gallery__lists">
+                <figure class="gallery__media">
+                    <img src="<?php echo get_template_directory_uri() ?>/uploads/gallery1.jpg" alt="image of a gallery" />
+                </figure>
+                <figure class="gallery__media">
+                    <img src="<?php echo get_template_directory_uri() ?>/uploads/gallery2.jpg" alt="image of a gallery" />
+                </figure>
+                <figure class="gallery__media">
+                    <img src="<?php echo get_template_directory_uri() ?>/uploads/gallery3.jpg" alt="image of a gallery" />
+                </figure>
+                <figure class="gallery__media gallery__media-hide">
+                    <img src="<?php echo get_template_directory_uri() ?>/uploads/gallery4.jpg" alt="image of a gallery" />
+                </figure>
+                <figure class="gallery__media gallery__media-last">
+                    <img src="<?php echo get_template_directory_uri() ?>/uploads/gallery5.jpg" alt="image of a gallery" />
+                </figure>
+            </div>
+        </div>
+    </section>
+    <?php include('modules/flex-content/flex-content.php') ?>
+</main>
+<?php get_footer(); ?>
---
+++ wp-content/themes/default/header.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
+
+<head>
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <?php wp_head(); ?>
+
+    <!-- Included via Site Options -->
+    <?php
+    if (get_field("site_header_code") != "") {
+        echo $get_field("site_header_code");
+    }
+    ?>
+    <!-- End Site Options -->
+
+</head>
+
+<body <?php body_class(); ?>>
+    <!-- Included via Site Options -->
+    <?php
+    if (get_field("site_header_code") != "") {
+        echo $get_field("site_body_code");
+    }
+    ?>
+    <!-- End Site Options -->
+    <header class="header js-header">
+        <div class="container">
+            <div class="logo__container">
+                <a href="<?php echo esc_url(home_url()); ?>" class="logo__link">
+                    <div class="logo__wrap">
+                        <img class="header__logo-image" src="<?php echo get_template_directory_uri() ?>/uploads/logoo.png" alt="Image of a logo" />
+                    </div>
+                    <meta itemprop="name" content="<?php bloginfo('name'); ?>">
+                </a>
+            </div>
+            <a href="#" class="mobileNav__toggle js-nav-toggle">
+                <span class="accessible-text">Click to toggle navigation menu.</span>
+                <div class="menuBar__container">
+                    <span class="menuBar"></span>
+                    <span class="menuBar"></span>
+                    <span class="menuBar"></span>
+                </div>
+            </a>
+            <?php
+            $menuArgs = array(
+                'container' => false,
+                'menu' => 'Main Navigation',
+                'menu_class' => 'primaryNav'
+            );
+            wp_nav_menu($menuArgs);
+            ?>
+
+            <div class="header-nav">
+                <div class="header-top">
+                    <div class="header-menu">
+                        <div class="header-socialmedia">
+                            <ul class="header-socialmedia__list">
+                                <li><a href="#" target="_blank"><svg class="icon icon-twitter">
+                                            <use xlink:href="#icon-twitter"></use>
+                                        </svg></a></li>
+                                <li><a href="#" target="_blank"><svg class="icon icon-fb">
+                                            <use xlink:href="#icon-fb"></use>
+                                        </svg></a></li>
+                                <li><a href="#" target="_blank"><svg class="icon icon-msg">
+                                            <use xlink:href="#icon-msg"></use>
+                                        </svg></a></li>
+                            </ul>
+                        </div>
+                        <div class="header-bandwidt">
+                            <div class="header-info">
+                                <p>Low Bandwidth</p>
+                                <div class="header-info__links">
+                                    <a href="#">A</a>
+                                    <a class="link-alt" href="#">A</a>
+                                </div>
+                            </div>
+                            <div class="header-info">
+                                <p>Screen Reader</p>
+                                <div class="header-info__list">
+                                    <a href="#">-</a>
+                                    <span>A</span>
+                                    <a href="#">+</a>
+                                </div>
+                            </div>
+                            <div class="header-language">
+                                <button>EN</button>
+                                <label class="switch">
+                                    <input type="checkbox" checked>
+                                    <span></span>
+                                </label>
+                                <button>NP</button>
+                            </div>
+                        </div>
+                    </div>
+                    <figure class="nepal">
+                        <img class="nepal__img" src="<?php echo get_template_directory_uri() ?>/uploads/flag.png" alt="Flag of Nepal" />
+                    </figure>
+                </div>
+                <div class="header-bottom">
+
+                    <ul class="main-title">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About <svg class="icon icon-arrow">
+                                    <use xlink:href="#icon-arrow"></use>
+                                </svg></a>
+                            <ul class="title">
+                                <li><a href="#">menu </a></li>
+                                <li><a href="#">menu </a></li>
+                                <li><a href="#">menu </a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Major Programs <svg class="icon icon-arrow">
+                                    <use xlink:href="#icon-arrow"></use>
+                                </svg></a>
+                            <ul class="title">
+                                <li><a href="#">menu </a></li>
+                                <li><a href="#">menu </a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Online Services <svg class="icon icon-arrow">
+                                    <use xlink:href="#icon-arrow"></use>
+                                </svg></a>
+                            <ul class="title">
+                                <li><a href="#">menu </a></li>
+                                <li><a href="#">menu </a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">More <svg class="icon icon-arrow">
+                                    <use xlink:href="#icon-arrow"></use>
+                                </svg></a>
+                            <ul class="title">
+                                <li><a href="#">menu </a></li>
+                                <li><a href="#">menu </a></li>
+                                <li><a href="#">menu </a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
